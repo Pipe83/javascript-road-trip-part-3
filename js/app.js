b4eaebdfa340b92ef125c4cc8b677be696bb1f69
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-25 21:56:38 -0400

    working on 1.7

--- js/app.js
+++ js/app.js
@@ -106,3 +106,22 @@ function confirmRide(confirmToGo) {
 
 // Call confirmRide with the fearMessage function
 var startRide = confirmRide(fearMessage);
+
+//-----------------------------//
+//--1.7 USING MAP WITH ARRAYS--//
+//-----------------------------//
+
+/* Over at Maple Mountain, the dev team has received some name data from the customer terminal that they want to use to customize greetings for each of their passengers. But the data consists of an array with many subarrays where the first and last names of each passenger are split up.
+
+They want to take the passengers array and convert those subarrays into strings that contain the first and last name for each passenger.
+
+Create a modifiedNames variable to store our new data.
+Assign passengers.map() to the modifiedNames variable. This will allow us to pass in a function to be used on every element in the array.
+Pass an anonymous function to map().
+The anonymous function should take in arrayCell as a parameter and use that to return a string with the first and last name for a passenger. In other words, if you were to pass in a ["Jason", "Millhouse"] array, the function should return a "Jason Millhouse" string. */
+
+
+var passengers = [ ["Thomas", "Meeks"],
+                   ["Gregg", "Pollack"],
+                   ["Christine", "Wong"],
+                   ["Dan", "McGaw"] ];
