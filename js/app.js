8f743f795e225ef20f12bc765c7266fcf4739901
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-26 22:12:08 -0400

    completed exercise 1.7

--- js/app.js
+++ js/app.js
@@ -125,3 +125,7 @@ var passengers = [ ["Thomas", "Meeks"],
                    ["Gregg", "Pollack"],
                    ["Christine", "Wong"],
                    ["Dan", "McGaw"] ];
+
+var modifiedNames = passengers.map(function (arrayCell) {return arrayCell[0] + " " + arrayCell[1];});
+
+console.log(modifiedNames);
