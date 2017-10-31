2d3c93bab769bd572c86a3b87f70dd0e638e3bd6
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-31 14:58:55 -0400

    completed exercise 2.2, some math involved, yay

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-//********************************//
-//--Forest of Function Expressions//
-//********************************//
+//**********************************//
+//--Forest of Function Expressions--//
+//**********************************//
 
 
 //--------------------------------------------//
@@ -328,3 +328,35 @@ var puzzlers = [
 
 alert(puzzlers[puzzlers[1](3)](puzzlers[3](9)));
 //That's just plain wrong ....
+
+
+
+//**********************//
+//--Cold Closures Cove--//
+//**********************//
+
+//---------------------------//
+//--2.2 TRACING A CLOSURE 1--//
+//---------------------------//
+
+/* Examine the code below (i.e., manually trace it), in your head determine the final value of the result variable, and alert the value as a number using one line of code.
+
+You must give only 1 number literal as the argument to alert.
+Do not do any calculations. So, for example, don’t use *. (Definitely do some math in your head, though!)
+Don’t pass in a string.
+The number must be a whole number. No decimal points.
+(This challenge will give you practice following code with closures, and allow you to understand it better.)
+
+function mystery() {
+  var secret = 6;
+  function mystery2(multiplier) {
+    multiplier *= 3;
+    return secret * multiplier;
+  }
+  return mystery2;
+}
+
+var hidden = mystery();
+var result = hidden(3); */
+
+alert(54);
