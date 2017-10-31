a2471f5df20867b023303c947a9184fb5fd72230
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-31 15:02:05 -0400

    completed exercise 2.3, more math

--- js/app.js
+++ js/app.js
@@ -336,7 +336,7 @@ alert(puzzlers[puzzlers[1](3)](puzzlers[3](9)));
 //**********************//
 
 //---------------------------//
-//--2.2 TRACING A CLOSURE 1--//
+//--2.2 TRACING A CLOSURE I--//
 //---------------------------//
 
 /* Examine the code below (i.e., manually trace it), in your head determine the final value of the result variable, and alert the value as a number using one line of code.
@@ -360,3 +360,23 @@ var hidden = mystery();
 var result = hidden(3); */
 
 alert(54);
+
+//----------------------------//
+//--2.3 TRACING A CLOSURE II--//
+//----------------------------//
+
+/* We’ve made the code a little trickier this time. Again, calculate the final value of the result variable and alert the value as a number using one line of code.
+
+function mystery(input) {
+  var secret = 5;
+  function mystery2(multiplier) {
+    multiplier *= input;
+    return secret * multiplier;
+  }
+  return mystery2;
+}
+
+var hidden = mystery(4);
+var result = hidden(2); */
+
+alert(40);
