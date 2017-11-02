eb6e7b9236c79eba97211f4bd7958e16823efe22
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-02 16:26:52 -0400

    completed exercise 2.8

--- js/app.js
+++ js/app.js
@@ -451,9 +451,9 @@ var icebergAlert = warningMaker("iceberg");
 icebergAlert();
 
 
-//--------------------------//
-//--2.6 USING A CLOSURE II--//
-//--------------------------//
+//-----------------------------//
+//--2.7 BUILDING A CLOSURE II--//
+//-----------------------------//
 
 /* The Dev Girls admire your closure mastery. To test your might, they’ve asked you to modify the warningMaker function in order to announce:
 
@@ -476,3 +476,40 @@ function warningMaker(obstacle) {
     );
   };
 }
+
+
+//--------------------------//
+//--2.8 USING A CLOSURE II--//
+//--------------------------//
+
+/* You’ve impressed the Dev Girls with the improved functionality of your warning messages. They’ve built a few warning generators and are ready for any danger that comes up. Suddenly, the radio crackles to life:
+
+Dev Girls, mayday. Dev Girls, mayday!
+
+I’ve got 6 killer penguins on the loose near the "Ice Caves"!
+
+And 1 snow yeti rampaging across the "Blizzard Beach"!
+
+Over and out!
+
+Review the already-built warning messages, and call the appropriate functions for this situation, making sure to pass in the appropriate parameters. You’ll need only two lines of code. */
+
+
+function warningMaker(obstacle) {
+  return function(number, location) {
+    alert("Beware! There have been " + obstacle +
+          " sightings in the Cove today!\n" +
+          number + " have been spotted at the " +
+          location + "!");
+  };
+}
+
+var killerPenguinAlert = warningMaker("killer penguin");
+var polarBearAlert     = warningMaker("polar bear");
+var icebergAlert       = warningMaker("iceberg");
+var flashBlizzardAlert = warningMaker("flash blizzard");
+var snowYetiAlert      = warningMaker("snow yeti");
+
+// call the two functions here
+killerPenguinAlert(6, "Ice Caves");
+snowYetiAlert(1, "Blizzard Beach");
