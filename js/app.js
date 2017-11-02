61c203de74bdeb01bee8c514483d19e5fe29e3cf
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-01 21:16:35 -0400

    completed exercise 2.4

--- js/app.js
+++ js/app.js
@@ -380,3 +380,33 @@ var hidden = mystery(4);
 var result = hidden(2); */
 
 alert(40);
+
+//-----------------------------//
+//--2.4 TRACING A CLOSURE III--//
+//-----------------------------//
+
+/* Moar tricky. Moar tough! Again, calculate the final value of the result variable and alert the value as a number using one line of code. Hehe, good luck with this one!
+
+function mystery(input) {
+  var secret = 4;
+  input += 2;
+  function mystery2(multiplier) {
+    multiplier *= input;
+    return secret * multiplier;
+  }
+  return mystery2;
+}
+
+function mystery3(param) {
+  function mystery4(bonus) {
+    return param(6) + bonus;
+  }
+  return mystery4;
+}
+
+var hidden = mystery(3);
+var jumble = mystery3(hidden);
+var result = jumble(2); */
+
+
+alert(122);
