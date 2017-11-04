94b3dd9552f040fd2da83429f08d7e3f16096719
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-03 22:34:58 -0400

    completed exercise 2.13

--- js/app.js
+++ js/app.js
@@ -637,3 +637,35 @@ function warningMaker(obstacle) {
           list);
   };
 }
+
+//------------------------------//
+//--2.13 FINAL CLOSED VALUES I--//
+//------------------------------//
+
+/* Now that the danger zones and obstacles are in order and ready to be dealt with, the Dev Girls need your assistance with directing the Cold Closures Cove sharks to their custom-fitted lasers. That’s right. Lasers.
+
+Sharks are supposed to be directed to the laser-strapping station that matches the index that their name is in within the sharkList array, which looks like this:
+
+["Sea Pain", "Great Wheezy", "DJ Chewie",
+ "Lil' Bitey", "Finmaster Flex", "Swim Khalifa",
+ "Ice Teeth", "The Notorious J.A.W."]
+But they’ve got a problem with their assignLaser function. Something is up, and now there’s a traffic jam at the last shark’s station.
+
+Figure out what happened and apply a fix from this section’s video lecture:
+
+Don’t change the position of the function inside the for loop.
+Instead of assigning to the stationAssignment variable, return the anonymous function.
+Remove the subsequently unnecessary lines with stationAssignment.
+Note: You do not need to call the assignLaser function. */
+
+function assignLaser(shark, sharkList) {
+  for (var i = 0; i < sharkList.length; i++) {
+    if (shark == sharkList[i]) {
+     		return function() {
+        alert("Yo, " + shark + "!\n" +
+              "Visit underwater strapping station " +
+              (i + 1) + " for your sweet laser.");
+      };
+    }
+  }
+}
