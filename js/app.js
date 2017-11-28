1b6d3ff9bf250f0e6bd29fe4394c27078d40ec42
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-28 12:37:04 -0500

    completed exercise 4.11

--- js/app.js
+++ js/app.js
@@ -1123,7 +1123,7 @@ function addRanger(location, name, skillz, station) {
   };
 } */
 
-/* var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
 
 var lighthouseRock = {
   gateClosed: true,
@@ -1134,9 +1134,12 @@ var lighthouseRock = {
   ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
   ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
   ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1},
+  addRanger: function ( name, skillz, station){
+    this[numRangers]++;
+    this[name]["ranger" + this.numRangers] = {name: name, skillz: skillz, station: station};
+  }
 
-
-}; */
+};
 
 //--------------------------------------//
 //--4.12 CALLING FUNCTION PROPERTIES I--//
