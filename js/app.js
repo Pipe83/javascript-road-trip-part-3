9bbf97a86dbeddaccb5e8647724eac777cd11cf6
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-12-01 23:10:17 -0500

    completed exercise 4.19, I understand that dot notation would have been better but bracket notation was a requirement for the exercise

--- js/app.js
+++ js/app.js
@@ -1326,7 +1326,7 @@ listGuns(rockSpearguns);
  }
 
  listGuns(rockSpearguns);
- 
+
  //------------------------//
  //--4.19 ENUMERATION III--//
  //------------------------//
@@ -1359,3 +1359,23 @@ function listGuns(guns) {
 
 // call listGuns using bracket notation on rockSpearguns
 listGuns(rockSpearguns); */
+
+var rockSpearguns = {
+  Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
+  Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
+  Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
+  Firefork: {barbs: 6, weight: 8, heft: "overhand"},
+  "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
+};
+
+// convert listGuns function
+rockSpearguns["listGuns"] = function () {
+  for (var property in this) {
+     if (this[property]["heft"] !== undefined) {
+      console.log("Behold! " + property + ", with " + this[property]["heft"] + " heft!");
+    }
+  }
+};
+
+// call listGuns using bracket notation on rockSpearguns
+rockSpearguns["listGuns"]();
