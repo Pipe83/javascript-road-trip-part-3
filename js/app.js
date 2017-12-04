8299c64ce402856c8c3cbdad0a4b9afcf7cc6c51
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-12-03 20:22:52 -0500

    completed exercise 5.3

--- js/app.js
+++ js/app.js
@@ -1412,9 +1412,61 @@ Note: You do not need to call the countCattle function. We’ll call it when tes
 Array.prototype.countCattle = function (kind){
 	var numKind;
   for(var i = 0; i < this.length; i++){
-    if(this.type === kind){
+    if(this[i].type == kind){
     	numKind++;
     }
   }
 	return numKind;
 };
+
+
+//---------------------//
+//--5.3 PROTOTYPES II--//
+//---------------------//
+
+/* The cowboy-devs have determined that they need to put the calves from the Canyon, the bulls from the Valley, and the cows from the Forest together in one pasture on the Plains.
+
+Using the countCattle function that you added to the Array prototype, build one line of code to alert the sum of:
+
+The number of canyonCows with a "calf" type.
+The number of valleyCows with a "bull" type.
+The number of forestCows with a "cow" type.
+The lists are provided here for your reference: */
+
+var canyonCows = [
+  {name: "Bessie", type: "cow", hadCalf: "Burt"},
+  {name: "Donald", type: "bull", hadCalf: null},
+  {name: "Esther", type: "calf", hadCalf: null},
+  {name: "Burt", type: "calf", hadCalf: null},
+  {name: "Sarah", type: "cow", hadCalf: "Esther"},
+  {name: "Samson", type: "bull", hadCalf: null},
+  {name: "Delilah", type: "cow", hadCalf: null}
+];
+var valleyCows = [
+  {name: "Danielle", type: "cow", hadCalf: null},
+  {name: "Brittany", type: "cow", hadCalf: "Christina"},
+  {name: "Jordan", type: "bull", hadCalf: null},
+  {name: "Trevor", type: "bull", hadCalf: null},
+  {name: "Christina", type: "calf", hadCalf: null},
+  {name: "Lucas", type: "bull", hadCalf: null}
+];
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+Array.prototype.countCattle = function(kind) {
+  var numKind = 0;
+  for (var i = 0; i < this.length; i++) {
+    if (this[i].type == kind) {
+      numKind++;
+    }
+  }
+  return numKind;
+};
+
+
+alert( canyonCows.countCattle("calf") + valleyCows.countCattle("bull") + forestCows.countCattle("cow"));
